9b92a72f9304a4d162d4a213563a5b8f3f916f2b
Vinicius Audibert <vini.audibert@gmail.com>
2021-10-20 23:01:12 -0300

    feat: adicionar página principal

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { App } from './App'
+
+import './styles/global.css'
+import { AuthProvider } from './context/auth'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
